c319d107716753e6c7f69b3e27b7df7f1682d1aa
khsung <khsung0@nate.com>
2021-01-14 17:28:14 +0900

    배열 선언

--- src/Array.java
+++ src/Array.java
@@ -1,9 +1,7 @@
 
 public class Array {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int[] intarray = new int[] { 1, 2, 3, 4, 5 };
+		char[] chararray = new char[] { 'a', 'b', 'c', 'd', 'e' };
 	}
-
 }
